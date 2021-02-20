ee503b0b9857895678b5a23857bc3f8f04329549
JuliaGood <gudak.yuliya@gmail.com>
2021-02-20 22:17:49 +0200

    DogLIST component was created

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
