01c056b41a70efdcd40610fbd418826ab4c373ca
JuliaGood <gudak.yuliya@gmail.com>
2022-02-19 12:17:12 +0200

    build of the project 7

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/dog-shelter'>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
